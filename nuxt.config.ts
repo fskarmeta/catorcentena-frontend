951984b58d158e2022a1d9b035c8093438aab952
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-05-06 23:37:33 +0200

    ADD: backend integration dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from "nuxt";
 import { resolve } from "pathe";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ["nuxt-windicss"],
+  buildModules: ["nuxt-windicss", "nuxt-graphql-client"],
   alias: {
     images: resolve(__dirname, "./assets/images"),
     style: resolve(__dirname, "./assets/style"),
@@ -14,4 +14,10 @@ export default defineNuxtConfig({
       typeCheck: true,
     },
   },
+  runtimeConfig: {
+    public: {
+      GQL_HOST: "http://localhost:1337/graphql",
+    }
+  },
+  baseUrl: "https://localhost:3000",
 });
