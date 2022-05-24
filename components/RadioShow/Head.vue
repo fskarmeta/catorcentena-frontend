a8083eb7d6fc6765282028e844c9757875ca6688
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-05-24 23:04:59 +0200

    ADD: Navigation from radioshow to artst

--- components/RadioShow/Head.vue
+++ components/RadioShow/Head.vue
@@ -11,8 +11,8 @@ defineProps({
     <div class="text-center">
         <h1 class="text-4xl my-5 text-cyan-300 font-bold">{{ title }}</h1>
         <h2>{{ description }}</h2>
-        <small  v-if="artists.length">
-           <span>Artists:</span> <UtilTagDivider :array="artists" child-name="artistName" tag="," />
+        <small class="mt-50px" v-if="artists.length">
+           <span>Artistas:</span> <UtilTagDivider :array="artists" child-name="artistName" tag="," :link="true" url="/artista/"/>
         </small>
     </div>
 </template>
--- components/Util/TagDivider.vue
+++ components/Util/TagDivider.vue
@@ -1,15 +1,40 @@
-<script lang="ts" setup>
-defineProps({
+<script setup>
+
+const router = useRouter()
+
+const props = defineProps({
     array: Array,
     childName: String,
     tag: String,
+    link: {
+        type: Boolean,
+        default: false,
+    },
+    url: {
+        type: String,
+        default: '',
+    },
+    routeAttr: {
+        type: String,
+        default: 'slug',
+    },
 });
+
+const { link, url, routeAttr } = toRefs(props);
+
+const navigateTo = (attr) => {
+    if (!link.value) {
+        return;
+    }
+    router.push(url.value + attr[routeAttr.value]);
+
+}
 
 </script>
 
 
 <template>
-    <span v-for="(child, index) in array">
+    <span v-for="(child, index) in array" @click="navigateTo(child.attributes)" :class="{ 'hover:cursor-pointer hover:text-green-200': link }">
         {{ child.attributes[childName] + `${array.length - 1 !== index ? ` ${tag} ` : ''}` }}
     </span>
 </template>
