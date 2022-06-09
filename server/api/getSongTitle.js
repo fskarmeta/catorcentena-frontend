1a185bd10556c53f51c3bf475c912f83e088f4c3
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-06-10 00:59:46 +0200

    ADD: try catch to get title

--- server/api/getSongTitle.js
+++ server/api/getSongTitle.js
@@ -1,6 +1,8 @@
 export default defineEventHandler(async () => {
-  const data = await $fetch('http://109.169.23.17:9090/status-json.xsl')
-  return {
-    title: data.icestats.source.title,
-  }
+  try {
+    const data = await $fetch('http://109.169.23.17:9090/status-json.xsl')
+    return {
+      title: data.icestats.source.title,
+    }
+  } catch {}
 })
