5e5f3b98883df794bce09a2cdeb825b500cafc78
Fabian Skarmeta <f.skarmeta@gmail.com>
2022-11-27 03:01:33 +0100

    add: sister radio component and view, view titles.

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@ const { data, pending } = await useAsyncGql('radioShows')
       <Title>Radioshows | 14.40 Radio</Title>
       <Meta name="description" content="Radio shows en catorcentena radio" />
     </Head>
+    <h1 class="mx-auto w-full text-3xl mb-15">Radio Shows</h1>
     <RadioShowCards
       v-if="data.radioShows"
       :radio-shows="data.radioShows.data"
---
+++ pages/radios-hermanas.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const { data, pending } = await useAsyncGql('sisterRadios')
+</script>
+
+<template>
+  <UtilSpinner v-if="pending" />
+
+  <Head>
+    <Title>Radios hermanas | 14.40 Radio</Title>
+    <Meta name="description" content="Radios hermanas de catorcentena radio" />
+  </Head>
+  <h1 class="mx-auto w-full text-3xl mb-15">Radios hermanas</h1>
+  <SisterRadio
+    v-for="radio in data?.sisterRadios?.data"
+    :key="radio.id!"
+    class="mb-20"
+    :title="radio?.attributes?.title!"
+    :description="radio?.attributes?.description || ''"
+    :image-url="radio?.attributes?.image?.data?.attributes?.formats?.small?.url || radio?.attributes?.image?.data?.attributes?.url!"
+    :link="radio?.attributes?.link!"
+  />
+</template>
