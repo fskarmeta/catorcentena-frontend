fbbc3c1df8404d8b1282d6f2ce65844206e6e89b
rambach369 <101401486+rambach369@users.noreply.github.com>
2023-05-08 19:13:56 +0000

    Update getSongTitle.js

--- server/api/getSongTitle.js
+++ server/api/getSongTitle.js
@@ -1,6 +1,6 @@
 export default defineEventHandler(async () => {
   try {
-    const data = await $fetch('http://109.169.23.123:8237/status-json.xsl')
+    const data = await $fetch('http://109.169.23.124:8288/status-json.xsl')
     return {
       title: data.icestats.source.title,
     }
