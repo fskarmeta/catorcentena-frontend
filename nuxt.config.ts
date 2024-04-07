2d6ba1a96df4e7275e9e7bd5ed73e0a2644c16c6
rambach369 <101401486+rambach369@users.noreply.github.com>
2024-04-07 13:22:38 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
     public: {
       GQL_HOST: 'https://catorcentena-graphql.herokuapp.com/graphql',
       RADIO_HOST:
-        'Fps3.listen2myradio.com:2199/listen.php?ip=109.169.23.124&port=8035&type=ice&mount=1440radio',
+        'Fps3.listen2myradio.com:2199/listen.php?ip=212.84.160.3&port=8830&type=ice&mount=1440radio',
     },
   },
   app: {
